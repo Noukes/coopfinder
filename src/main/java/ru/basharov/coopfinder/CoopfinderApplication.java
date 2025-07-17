ff09f3a724c2c9f283e1143dd28b9e161e6053af
Noukes <basharovv.ar@gmail.com>
2025-07-17 17:32:20 +0400

    Initial commit

---
+++ src/main/java/ru/basharov/coopfinder/CoopfinderApplication.java
@@ -0,0 +1,13 @@
+package ru.basharov.coopfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoopfinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoopfinderApplication.class, args);
+	}
+
+}
